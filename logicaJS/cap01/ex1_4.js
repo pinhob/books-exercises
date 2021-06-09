e38f199cec1c87f6bc734c2198be9aee26d5f1f7
Bruno Pinho <brunopinho@outlook.com>
2021-06-09 12:13:55 -0300

    criação de cap01 e exercícios 1 ao 8 do capítulo

---
+++ logicaJS/cap01/ex1_4.js
@@ -0,0 +1,10 @@
+//Convertendo a string em número
+let a = '20';
+let b = a * 2; // b = 40;
+let c = a / 2; // c = 10;
+let d = a - 2; // d = 18;
+let e = a + 2; 
+let f = Number(a); //Converte a string da variável a para um número
+let g  = f + 2; 
+console.log ('e: ' + e);
+console.log ('g: ' + g);
