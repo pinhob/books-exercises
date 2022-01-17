457dda21f6ff56a0d418908be53452a0808fe20e
Bruno Pinho <brunopinho@outlook.com>
2022-01-17 18:05:10 -0300

    feito códigos do capitulo 4 e capitulo 5

--- cangaceiro-javascript/client/app/controllers/NegociacaoController.js
+++ cangaceiro-javascript/client/app/controllers/NegociacaoController.js
@@ -6,24 +6,29 @@ class NegociacaoController {
     this._inputData = $('#data');
     this._inputValor = $('#valor');
     this._inputQuantidade = $('#quantidade');
-
+    this._negociacoes = new Negociacoes();
   }
 
   adiciona(event) {
     event.preventDefault();
 
-    let converter = new DateConverter();
+    this._negociacoes.adiciona(this._criaNegociacao());
 
-    let data = converter.paraData(this._inputData.value);
+    this._limpaFormulario();
+  }
 
-    let negociacao = new Negociacao(
-      data,
+  _limpaFormulario() {
+    this._inputData.value = '';
+    this._inputQuantidade.value = 1;
+    this._inputValor.value = 0.0;
+    this._inputData.focus();
+  }
+
+  _criaNegociacao() {
+    return new Negociacao(
+      DateConverter.paraData(this._inputData.value),
       parseInt(this._inputQuantidade.value),
       parseFloat(this._inputValor.value)
     );
-    
-    let diaMesAno = converter.paraTexto(negociacao.data);
-
-    console.log(diaMesAno);
   }
 };
--- cangaceiro-javascript/client/app/ui/converters/DateConverter.js
+++ cangaceiro-javascript/client/app/ui/converters/DateConverter.js
@@ -1,15 +1,22 @@
-class DateConverter { 
-  paraTexto(data) {
-    return data.getDate()
-      + '/' + (data.getMonth() + 1)
-      + '/' + data.getFullYear();
+class DateConverter {
+  constructor() {
+    throw new Error('Esta classe não pode ser instanciada');
   }
 
-  paraData(texto) {
+  static paraTexto(data) {
+    return `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`;
+  }
+
+  static paraData(texto) {
+    const validRegex = /^\d{4}-\d{2}-\d{2}$/;
+
+    if(!validRegex.test(texto))
+      throw new Error('Deve estar no formato aaaa-mm-dd');
+
     return new Date(
       ...texto
-      .split('-')
-      .map((item, indice) => item - indice % 2)
+        .split('-')
+        .map((item, indice) => item - indice % 2)
     );
   }
 }
